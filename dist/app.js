f873d594308dc5b5e7bd90f5f8984493feb82d5f
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-04-16 03:10:40 +0545

    install http-error and user throw error for this package

--- dist/app.js
+++ dist/app.js
@@ -4,13 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const http_errors_1 = __importDefault(require("http-errors"));
 const app = (0, express_1.default)();
 const port = 8000;
 // root directory
 app.get("/", (req, res) => {
-    res.send("Hello, world!");
+    res.json({
+        message: "Hello world!",
+    });
 });
-// Listen
+// Middleware
+app.use(() => {
+    throw (0, http_errors_1.default)(404, "Route not found!!");
+});
+// Server Created for port listening
 app.listen(port, () => {
     console.log(`Server listening on Port ${port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express from "express";
+import createHttpError from "http-errors";
 const app = express();
 const port = 8000;
 
 // root directory
 app.get("/", (req, res) => {
-	res.send("Hello, world!");
+	res.json({
+		message: "Hello world!",
+	});
 });
 
-// Listen
+// Middleware
+app.use(() => {
+	throw createHttpError(404, "Route not found!!");
+});
+
+// Server Created for port listening
 app.listen(port, () => {
 	console.log(`Server listening on Port ${port}`);
 });
