fd3face2c996693a2d0a68cb2142177ca1db66ec
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-09-09 05:17:21 +0545

    update files.

--- dist/routes/exampleRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const exampleControllers_1 = require("../controllers/exampleControllers");
-const exampleValidation_1 = require("../validation/exampleValidation/exampleValidation");
-const router = (0, express_1.Router)();
-router.get("/", exampleControllers_1.getExample);
-router.post("/", exampleValidation_1.getExampleDataValidation, exampleControllers_1.getExampleData);
-exports.default = router;
