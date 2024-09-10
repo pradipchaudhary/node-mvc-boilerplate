94cd64bcea1f7af9b1e7a029892d15084c8d6386
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-09-10 22:18:17 +0545

    Delete src directory

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-// src/app.ts
-console.log("Hello, TypeScript with Node.js!");
-console.log("This is the TypeScript Script ");
-function printName() {
-    console.log("Print Name functionhh...");
-}
-
-printName();
